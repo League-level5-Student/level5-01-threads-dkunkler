0825718181d9d09c53f0c49fcabfbbc5b152117a
David Kunkler <dkunkler@github.com>
2020-05-06 20:03:35 -0700

    Threadpool WIP

--- src/_04_Thread_Pool/ThreadPool.java
+++ src/_04_Thread_Pool/ThreadPool.java
@@ -1,5 +1,17 @@
 package _04_Thread_Pool;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class ThreadPool {
+Thread [] threads;
+ConcurrentLinkedQueue<Task> taskQueue;
+
+ThreadPool(int totalThreads){
+	threads = new Thread[totalThreads];
+	for(int i = 0; i<totalThreads; i++) {
+		threads[i] = new Thread();
+	}
+	taskQueue = new ConcurrentLinkedQueue<Task>();
+}
 
 }
