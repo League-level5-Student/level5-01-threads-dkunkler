4dd1ab0462bedcf36b0db66bdb8b913a5936fe9d
David Kunkler <dkunkler@github.com>
2020-05-10 12:07:32 -0700

    Thread Pool complete.
    starting threads and joining them in the same for each loop cut time in half from starting all threads then joining all threads.

--- src/_04_Thread_Pool/ThreadPool.java
+++ src/_04_Thread_Pool/ThreadPool.java
@@ -8,10 +8,31 @@ ConcurrentLinkedQueue<Task> taskQueue;
 
 ThreadPool(int totalThreads){
 	threads = new Thread[totalThreads];
-	for(int i = 0; i<totalThreads; i++) {
-		threads[i] = new Thread();
-	}
 	taskQueue = new ConcurrentLinkedQueue<Task>();
+	for(int i = 0; i<totalThreads; i++) {
+		threads[i] = new Thread(new Worker(taskQueue));
+	}
+
 }
+
+public void start()
+{
+	for( Thread t : threads) {
+		t.start();
+		try {
+	
+			t.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
+public void addTask(Task task) {
+	// TODO Auto-generated method stub
+	taskQueue.add(task);
+}
+
+
 
 }
